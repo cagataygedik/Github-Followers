a7829822ce202de392038e6d2310875bce7396fd
cagatay <cagatay.gedik@icloud.com>
2023-01-30 14:48:39 +0300

    Initial Commit

---
+++ Github Followers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Github Followers
+//
+//  Created by Celil Çağatay Gedik on 30.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
