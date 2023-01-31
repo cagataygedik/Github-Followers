df038e51615387138b131a760d13051d06dd1af8
cagatay <cagatay.gedik@icloud.com>
2023-01-31 15:29:29 +0300

    Created Button and TextField

---
+++ Github Followers/GFButton.swift
@@ -0,0 +1,34 @@
+//
+//  GFButton.swift
+//  Github Followers
+//
+//  Created by Celil Çağatay Gedik on 31.01.2023.
+//
+
+import UIKit
+
+class GFButton: UIButton {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(backgroundColor: UIColor, title: String) {
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        self.setTitle(title, for: .normal)
+        configure()
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        titleLabel?.textColor = .white
+        titleLabel?.font = UIFont.preferredFont(forTextStyle: .headline)
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+}
