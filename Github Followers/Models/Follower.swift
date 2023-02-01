55e71f2a718295337a920548cab97ac8bf45b2c0
cagatay <cagatay.gedik@icloud.com>
2023-02-01 12:17:02 +0300

    Created models

---
+++ Github Followers/Models/Follower.swift
@@ -0,0 +1,13 @@
+//
+//  Followers.swift
+//  Github Followers
+//
+//  Created by Celil Çağatay Gedik on 1.02.2023.
+//
+
+import Foundation
+
+struct Follower: Codable {
+    var login: String
+    var avatarUrl: String
+}
---
+++ Github Followers/Models/User.swift
@@ -0,0 +1,22 @@
+//
+//  User.swift
+//  Github Followers
+//
+//  Created by Celil Çağatay Gedik on 1.02.2023.
+//
+
+import Foundation
+
+struct User: Codable {
+    var login: String
+    var avatarUrl: String
+    var name: String?
+    var location: String?
+    var bio: String?
+    var publicRepos: Int
+    var publicGists: Int
+    var htmlUrl: String
+    var following: Int
+    var followers: Int
+    var createdAt: String
+}
