0e17f988769d237a69e05e402b3fa54d03479834
cagatay <cagatay.gedik@icloud.com>
2023-02-01 11:34:23 +0300

    Created Title Label for the Alert view.

---
+++ Github Followers/GFTitleLabel.swift
@@ -0,0 +1,35 @@
+//
+//  GFTitleLabel.swift
+//  Github Followers
+//
+//  Created by Celil Çağatay Gedik on 1.02.2023.
+//
+
+import UIKit
+
+class GFTitleLabel: UILabel {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(textAlignment: NSTextAlignment, fontSize: CGFloat) {
+        super.init(frame: .zero)
+        self.textAlignment = textAlignment
+        self.font = UIFont.systemFont(ofSize: fontSize, weight: .bold)
+        configure()
+    }
+    
+    private func configure() {
+        textColor = .label
+        adjustsFontSizeToFitWidth = true
+        minimumScaleFactor = 0.9
+        lineBreakMode = .byTruncatingHead
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+}
