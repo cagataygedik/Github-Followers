37ff5a6204f8fe56045255e2b2819f8b7e4848be
cagatay <cagatay.gedik@icloud.com>
2023-02-01 11:34:09 +0300

    Created Body Label for the Alert view.

---
+++ Github Followers/GFBodyLabel.swift
@@ -0,0 +1,35 @@
+//
+//  GFBodyLabel.swift
+//  Github Followers
+//
+//  Created by Celil Çağatay Gedik on 1.02.2023.
+//
+
+import UIKit
+
+class GFBodyLabel: UILabel {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(textAlignment: NSTextAlignment) {
+        super.init(frame: .zero)
+        self.textAlignment = textAlignment
+        configure()
+    }
+    
+    private func configure() {
+        textColor = .secondaryLabel
+        font = UIFont.preferredFont(forTextStyle: .body)
+        adjustsFontSizeToFitWidth = true
+        minimumScaleFactor = 0.75
+        lineBreakMode = .byWordWrapping
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+}
