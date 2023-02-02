df23acceaaf23e6c59e6d2b325421e2b2085f039
cagatay <cagatay.gedik@icloud.com>
2023-02-02 21:49:32 +0300

    Refactored the error message and response types

--- Github Followers/Screens/FollowerListViewController.swift
+++ Github Followers/Screens/FollowerListViewController.swift
@@ -16,13 +16,16 @@ class FollowerListViewController: UIViewController {
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
         
-        NetworkManager.shared.getFollowers(for: userName, page: 1) { (followers, errorMessage) in
-            guard let followers = followers else {
-                self.presentGFAlertOnMainThread(alertTitle: "Bad stuff happened", message: errorMessage!, buttonTitle: "Okay")
-                return
+        NetworkManager.shared.getFollowers(for: userName, page: 1) { result in
+            
+            switch result {
+                
+            case .success(let followers):
+                print(followers)
+                
+            case .failure(let error):
+                self.presentGFAlertOnMainThread(alertTitle: "Bad stuff happened", message: error.rawValue, buttonTitle: "Try Again")
             }
-            print("Followers.count = \(followers.count)")
-            print(followers)
         }
     }
     
