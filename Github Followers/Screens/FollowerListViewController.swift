6d304704cb2fa6650a6ee5039d397544b1b9f96b
cagatay <cagatay.gedik@icloud.com>
2023-02-02 21:17:47 +0300

    Testing our network service.

--- Github Followers/Screens/FollowerListViewController.swift
+++ Github Followers/Screens/FollowerListViewController.swift
@@ -10,11 +10,20 @@ import UIKit
 class FollowerListViewController: UIViewController {
     
     var userName: String!
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
+        
+        NetworkManager.shared.getFollowers(for: userName, page: 1) { (followers, errorMessage) in
+            guard let followers = followers else {
+                self.presentGFAlertOnMainThread(alertTitle: "Bad stuff happened", message: errorMessage!, buttonTitle: "Okay")
+                return
+            }
+            print("Followers.count = \(followers.count)")
+            print(followers)
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
