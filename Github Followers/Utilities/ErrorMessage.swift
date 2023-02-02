1a62c7bf0987af6e437b019f472d4c7a6fc31a6d
cagatay <cagatay.gedik@icloud.com>
2023-02-02 21:49:16 +0300

    Created custom enum.

---
+++ Github Followers/Utilities/ErrorMessage.swift
@@ -0,0 +1,15 @@
+//
+//  ErrorMessage.swift
+//  Github Followers
+//
+//  Created by Celil Çağatay Gedik on 2.02.2023.
+//
+
+import Foundation
+
+enum GFError: String, Error {
+    case invalidUsername = "This username created an invalid request. Please try again."
+    case unableToComplete = "Unable to complete your request. Please check your internet connection"
+    case invalidResponse = "Invalid response from the server. Please try again."
+    case invalidData = "The data received from the server was invalid. Please try again."
+}
