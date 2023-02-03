b3d26e423164798f992a04106047587d3d56c70c
cagatay <cagatay.gedik@icloud.com>
2023-02-03 12:46:06 +0300

    Created avatar image for Follower Cell.

---
+++ Github Followers/Custom Views/GFAvatarImageView.swift
@@ -0,0 +1,30 @@
+//
+//  GFAvatarImageView.swift
+//  Github Followers
+//
+//  Created by Celil Çağatay Gedik on 3.02.2023.
+//
+
+import UIKit
+
+class GFAvatarImageView: UIImageView {
+    
+    let placeholderImage = UIImage(named: "avatar-placeholder")!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        layer.cornerRadius = 10
+        clipsToBounds = true
+        image = placeholderImage
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+}
