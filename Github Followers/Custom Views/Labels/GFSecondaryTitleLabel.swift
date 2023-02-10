b4a3fe3ca976f75d10371abe5779ae044224853e
cagatay <cagatay.gedik@icloud.com>
2023-02-10 14:18:02 +0300

    Created new SecondaryTitleLabel.

---
+++ Github Followers/Custom Views/Labels/GFSecondaryTitleLabel.swift
@@ -0,0 +1,33 @@
+//
+//  GFSecondaryTitleLabel.swift
+//  Github Followers
+//
+//  Created by Celil Çağatay Gedik on 10.02.2023.
+//
+
+import UIKit
+
+class GFSecondaryTitleLabel: UILabel {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(fontSize: CGFloat) {
+        super.init(frame: .zero)
+        font = UIFont.systemFont(ofSize: fontSize, weight: .medium)
+        configure()
+    }
+    
+    private func configure() {
+        textColor = .secondaryLabel
+        adjustsFontSizeToFitWidth = true
+        minimumScaleFactor = 0.90
+        lineBreakMode = .byTruncatingTail
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
