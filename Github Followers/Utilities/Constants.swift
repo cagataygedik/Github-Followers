c9de9fc41e02d1630cbc9433579912a94d430752
cagatay <cagatay.gedik@icloud.com>
2023-02-10 14:18:26 +0300

    Created constants for future using.

---
+++ Github Followers/Utilities/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Github Followers
+//
+//  Created by Celil Çağatay Gedik on 10.02.2023.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
