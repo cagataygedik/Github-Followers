d50ac88fdb72dd757d1f12a0e5c2fcf54b7222b2
cagatay <cagatay.gedik@icloud.com>
2023-02-12 15:24:49 +0300

    Created ItemInfoViewController subclasses.

---
+++ Github Followers/Custom Views/ViewControllers/ItemInfoViewControllers/GFFollowerItemViewController.swift
@@ -0,0 +1,22 @@
+//
+//  GFFollowerItemViewController.swift
+//  Github Followers
+//
+//  Created by Celil Çağatay Gedik on 12.02.2023.
+//
+
+import UIKit
+
+class GFFollowerItemViewController: GFItemInfoViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureItems()
+    }
+    
+    private func configureItems() {
+        itemInfoViewOne.set(itemInfoType: .followers, withCount: user.followers)
+        itemInfoViewTwo.set(itemInfoType: .following, withCount: user.following)
+        actionButton.set(backgroundColor: .systemGreen, title: "Get Followers")
+    }
+}
---
+++ Github Followers/Custom Views/ViewControllers/ItemInfoViewControllers/GFRepoItemViewController.swift
@@ -0,0 +1,22 @@
+//
+//  GFRepoItemViewController.swift
+//  Github Followers
+//
+//  Created by Celil Çağatay Gedik on 12.02.2023.
+//
+
+import UIKit
+
+class GFRepoItemViewController: GFItemInfoViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureItems()
+    }
+    
+    private func configureItems() {
+        itemInfoViewOne.set(itemInfoType: .repos, withCount: user.publicRepos)
+        itemInfoViewTwo.set(itemInfoType: .gists, withCount: user.publicGists)
+        actionButton.set(backgroundColor: .systemPurple, title: "Github Profile")
+    }
+}
