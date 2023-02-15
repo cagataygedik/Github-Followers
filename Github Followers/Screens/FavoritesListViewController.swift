212a5475aa09092324d8e44bafae44b5f43666b0
cagatay <cagatay.gedik@icloud.com>
2023-02-15 18:13:41 +0300

    Testing favorites list.

--- Github Followers/Screens/FavoritesListViewController.swift
+++ Github Followers/Screens/FavoritesListViewController.swift
@@ -11,6 +11,16 @@ class FavoritesListViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemRed
+        view.backgroundColor = .systemBackground
+        
+        //Debugging code
+        PersistanceManager.retrieveFavorites { result in
+            switch result {
+            case .success(let favorites):
+                print(favorites)
+            case .failure(let error):
+                break
+            }
+        }
     }
 }
