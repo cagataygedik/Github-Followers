d31bcab65e31972c19f1780e3825388768b42abb
cagatay <cagatay.gedik@icloud.com>
2023-02-16 18:34:23 +0300

    Created new enum for logo.

--- Github Followers/Utilities/Constants.swift
+++ Github Followers/Utilities/Constants.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import UIKit
 
 enum SFSymbols {
     static let location = "mappin.and.ellipse"
@@ -13,4 +14,8 @@ enum SFSymbols {
     static let gists = "text.alignleft"
     static let followers = "heart"
     static let following = "person.2"
+}
+
+enum Images {
+    static let ghLogo = UIImage(named: "gh-logo")
 }
