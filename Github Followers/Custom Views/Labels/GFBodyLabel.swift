d9e5fde73f771ead11fa98fc966621a0dd575009
cagatay <cagatay.gedik@icloud.com>
2023-02-16 19:20:09 +0300

    Code optimazation.

--- Github Followers/Custom Views/Labels/GFBodyLabel.swift
+++ Github Followers/Custom Views/Labels/GFBodyLabel.swift
@@ -11,16 +11,16 @@ class GFBodyLabel: UILabel {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        configure()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    init(textAlignment: NSTextAlignment) {
-        super.init(frame: .zero)
+    convenience init(textAlignment: NSTextAlignment) {
+        self.init(frame: .zero)
         self.textAlignment = textAlignment
-        configure()
     }
     
     private func configure() {
--- Github Followers/Custom Views/Labels/GFSecondaryTitleLabel.swift
+++ Github Followers/Custom Views/Labels/GFSecondaryTitleLabel.swift
@@ -11,16 +11,17 @@ class GFSecondaryTitleLabel: UILabel {
     
     override init(frame: CGRect) {
         super.init(frame: frame)
+        configure()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    init(fontSize: CGFloat) {
-        super.init(frame: .zero)
+    convenience init(fontSize: CGFloat) {
+        self.init(frame: .zero)
         font = UIFont.systemFont(ofSize: fontSize, weight: .medium)
-        configure()
+        
     }
     
     private func configure() {
