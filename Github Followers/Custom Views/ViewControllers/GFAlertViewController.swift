99e239a9e94790a5da7ef0501506ca6c951cbf11
cagatay <cagatay.gedik@icloud.com>
2023-02-16 19:19:58 +0300

    Moved ContainerView configuration to the new file.

--- Github Followers/Custom Views/ViewControllers/GFAlertViewController.swift
+++ Github Followers/Custom Views/ViewControllers/GFAlertViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class GFAlertViewController: UIViewController {
     
-    let containerView = UIView()
+    let containerView = GFAlertContainerView()
     let titleLabel = GFTitleLabel(textAlignment: .center, fontSize: 20)
     let bodyLabel = GFBodyLabel(textAlignment: .center)
     let actionButton = GFButton(backgroundColor: .systemPink, title: "Okay")
@@ -34,7 +34,7 @@ class GFAlertViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = UIColor(red: 0, green: 0, blue: 0, alpha: 0.75)
+        view.backgroundColor = UIColor.black.withAlphaComponent(0.75)
         configueContainerView()
         configureTitlelabel()
         configureActionButton()
@@ -43,13 +43,7 @@ class GFAlertViewController: UIViewController {
     
     func configueContainerView() {
         view.addSubview(containerView)
-        
-        containerView.backgroundColor = .systemBackground
-        containerView.layer.cornerRadius = 16
-        containerView.layer.borderWidth = 2
-        containerView.layer.borderColor = UIColor.white.cgColor
-        containerView.translatesAutoresizingMaskIntoConstraints = false
-        
+    
         NSLayoutConstraint.activate([
             containerView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
             containerView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
---
+++ Github Followers/Custom Views/Views/GFAlertContainerView.swift
@@ -0,0 +1,29 @@
+//
+//  GFAlertContainerView.swift
+//  Github Followers
+//
+//  Created by Celil Çağatay Gedik on 16.02.2023.
+//
+
+import UIKit
+
+class GFAlertContainerView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure() {
+        backgroundColor = .systemBackground
+        layer.cornerRadius = 16
+        layer.borderWidth = 2
+        layer.borderColor = UIColor.white.cgColor
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+    
+}
