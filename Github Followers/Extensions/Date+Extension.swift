8cad950be3b64fc1b74859e607ad91af96f3e343
cagatay <cagatay.gedik@icloud.com>
2023-02-20 11:52:26 +0300

    Refactored date formatter.

--- Github Followers/Extensions/Date+Extension.swift
+++ Github Followers/Extensions/Date+Extension.swift
@@ -8,9 +8,14 @@
 import Foundation
 
 extension Date {
+    /* OLD ONE
     func convertToMonthYearFormat() -> String {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "MMM yyyy"
         return dateFormatter.string(from: self)
+    }
+     */
+    func convertToMonthYearFormat() -> String {
+        return formatted(.dateTime.month().year())
     }
 }
